2a36d39a3e22c98243b265379a4e253b19994a96
javi <javier@e-zense.com>
2017-01-13 14:13:26 -0300

    Movies html
    Suma movies. Faltan algunos checks antes del submit y deberíamos
    ponernos de acuerdo si genero un Json con todo o si va en el POST..
    veamos.

--- html/js/movie.js
+++ html/js/movie.js
@@ -1,25 +1,162 @@
 $( document ).ready(function() {
-        console.log( "ready!" );
-       
+    
+    console.log( "ready!" );
+    
+    var clickedVal; // recoge el valor del ID seleccionado en la lista de movies;
+    
     // simular exit con el botó de salir
     $("#exitBtn").click(function(){
            window.location.href = "index.html?logstatus=OFF";
        })
     
-    // navega desde el selector de conenidos al value+".html" de cada option
+    // Toma el ID de la movie seleccionada en la lista
     $( "#movie-select" ).change(function() {
-        var clickedVal;
+        
         $( "#movie-select option:selected" ).each(function() {
           clickedVal= $(this).val();
             if(clickedVal!="")
             {
-                console.log( "clickedVal="+clickedVal );
-                $( "#movieID" ).val(clickedVal);
+                console.log( "clickedVal="+clickedVal ); // debug
+                $( "#movieID" ).val(clickedVal);// Agrega el ID en el input field
+                changeVideo(clickedVal, "#repro1");
+                
             }
         });
         
     });
     
+    // interacción del usuario al hacer click en el botón debajo de la lista de selección
+    $( "#IDBtn" ).click(function(){ 
+        $( "#idTit" ).html("AGREGANDO ID: "+clickedVal);// Agrega el ID en el título
+        $( "#hidden1" ).show();
+    })
+    
+    //preview de imagenes cargadas por el front end
+    
+    $( "#ThumbHor" ).change(showPreviewImage_click);
+    $( "#ThumbVer" ).change(showPreviewImage_click);
+    
     // inicializar tooltips para los íconos de HELP
     $('[data-toggle="tooltip"]').tooltip(); 
+    
+    
+    // drag and drop controles para las listas
+    var adjustment;
+
+    $("ul.pornstarPick").sortable({
+      group: 'pornstarPick',
+      pullPlaceholder: false,
+      // animation on drop
+      onDrop: function  ($item, container, _super) {
+        var $clonedItem = $('<li/>').css({height: 0});
+        $item.before($clonedItem);
+        $clonedItem.animate({'height': $item.height()});
+
+        $item.animate($clonedItem.position(), function  () {
+          $clonedItem.detach();
+          _super($item, container);
+        });
+      },
+
+      // set $item relative to cursor position
+      onDragStart: function ($item, container, _super) {
+        var offset = $item.offset(),
+            pointer = container.rootGroup.pointer;
+
+        adjustment = {
+          left: pointer.left - offset.left,
+          top: pointer.top - offset.top
+        };
+
+        _super($item, container);
+      },
+      onDrag: function ($item, position) {
+        $item.css({
+          left: position.left - adjustment.left,
+          top: position.top - adjustment.top
+        });
+      }
+    });
+    
+    
+    /**/
+    $("ul.generoPick").sortable({
+      group: 'generoPick',
+      pullPlaceholder: false,
+      // animation on drop
+      onDrop: function  ($item, container, _super) {
+        var $clonedItem = $('<li/>').css({height: 0});
+        $item.before($clonedItem);
+        $clonedItem.animate({'height': $item.height()});
+
+        $item.animate($clonedItem.position(), function  () {
+          $clonedItem.detach();
+          _super($item, container);
+        });
+      },
+
+      // set $item relative to cursor position
+      onDragStart: function ($item, container, _super) {
+        var offset = $item.offset(),
+            pointer = container.rootGroup.pointer;
+
+        adjustment = {
+          left: pointer.left - offset.left,
+          top: pointer.top - offset.top
+        };
+
+        _super($item, container);
+      },
+      onDrag: function ($item, position) {
+        $item.css({
+          left: position.left - adjustment.left,
+          top: position.top - adjustment.top
+        });
+      }
+    });
+    
+    
+    // checkbox idiomas
+    $("input[type=checkbox]").on('change', function () {
+        var self = $(this);
+        var showDiv = "#Module_"+self.attr("id");
+        if (self.is(":checked")) {
+            console.log("checkbox  id =" + self.attr("id") + "is checked ");
+            $(showDiv).show('slow');
+        } else {
+            console.log("Id = " + self.attr("id") + "is Unchecked ");
+            $(showDiv).hide();
+        }
+        
+    });
+    
+//----------------> Helper functions
+    
+    function changeVideo(src, divId){// para cambiar lo que reproduce el player de acuerdo al ID de la lista.
+        path="video/";
+        var video = $(divId+' video')[0];
+        video.src = path+src+".mp4";
+        console.log(video.src);
+        video.load();
+        video.play();
+    }
+    
+    function showPreviewImage_click(e) {
+            var $input = $(this);
+            var inputFiles = this.files;
+            if(inputFiles == undefined || inputFiles.length == 0) return;
+            var inputFile = inputFiles[0];
+
+            var reader = new FileReader();
+            reader.onload = function(event) {
+                $input.next().attr("src", event.target.result);
+            };
+            reader.onerror = function(event) {
+                alert("I AM ERROR: " + event.target.error.code);
+            };
+            reader.readAsDataURL(inputFile);
+    }
+    
+    
+    
 });
