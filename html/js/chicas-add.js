df44f058b32f313c3949d0f67788097036a68fc6
javi <javier@e-zense.com>
2017-02-02 14:57:00 -0300

    Chicas - Corrección de JS
    Agregado de función para mostrar thumbnails.

--- html/js/chicas-add.js
+++ html/js/chicas-add.js
@@ -15,6 +15,11 @@ $( document ).ready(function() {
         window.location.href = "chicas.html";
     })
     
+    //preview de imagenes cargadas por el front end
+    
+    $( "#ThumbHor" ).change(showPreviewImage_click);
+    $( "#ThumbVer" ).change(showPreviewImage_click);
+    
      // checkbox idiomas detecta lo que se chequea y muestra el módulo de idioma
     $("input[type=checkbox]").on('change', function () {
         var self = $(this);
@@ -29,3 +34,19 @@ $( document ).ready(function() {
         
     });   
 });
+
+function showPreviewImage_click(e) {
+            var $input = $(this);
+            var inputFiles = this.files;
+            if(inputFiles == undefined || inputFiles.length == 0) return;
+            var inputFile = inputFiles[0];
+
+            var reader = new FileReader();
+            reader.onload = function(event) {
+                $input.next().attr("src", event.target.result);
+            };
+            reader.onerror = function(event) {
+                alert("ERROR: " + event.target.error.code);
+            };
+            reader.readAsDataURL(inputFile);
+    }
