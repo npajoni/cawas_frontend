4bbcc7dc3a8f0d9c178389554a1f280b85b8409e
javi <javier@e-zense.com>
2017-02-13 09:47:11 -0300

    Movies - testing
    Versión de prueba JSON + files. NO ES LA FINAL, la versión final sale en
    2 horas. A esta le falta la revisión de los arrays.
    Se agregaron los campos requeridos por el data model : Año de estreno y
    directores.
    Se agregaron date-pickers en la elección de fecha de publicación de cada
    idioma.
    Se cambió completamente el sistema de chequeo de los campos. Se hizo uno
    custom para poder enviar solamente los dos archivos de imágenes y el
    string de JSON en POST. Estamos debuggeando el armado del JSON en los
    arrays (pornstar, categorías) e idiomas. El próximo commit tendrá la
    versión final, con este se puede testear el manejo de los archivos de
    imagen y el parseo del JSON ya que se envía entero con dummie-data donde
    hay arrays.

--- html/js/movie.js
+++ html/js/movie.js
@@ -1,7 +1,10 @@
 $( document ).ready(function() {
     
     console.log( "ready!" );
-    
+    var checkedOnce = 0; //chequea si el formulario se ha intentado enviar alguna vez
+    var checkVal=0; //chequea la cantidad de errores en el formulario
+    var clickedToSubmit = 0; //chequea si el botón de submit se ha presionado
+    var checkText="";
     var clickedVal; // recoge el valor del ID seleccionado en la lista de movies;
     
     // simular exit con el botó de salir
@@ -116,6 +119,17 @@ $( document ).ready(function() {
     });
     
     
+    /*---- DATE PICKER ----*/
+    $('.datePick').dcalendarpicker({
+     // default: mm/dd/yyyy
+
+      format: 'dd-mm-yyyy'
+
+    });
+
+    
+    
+    
     // checkbox idiomas detecta lo que se chequea y muestra el módulo de idioma
     $("input[type=checkbox]").on('change', function () {
         var self = $(this);
@@ -149,7 +163,7 @@ $( document ).ready(function() {
 
             var reader = new FileReader();
             reader.onload = function(event) {
-                $input.next().attr("src", event.target.result);
+                $input.parent().find('img').attr("src", event.target.result);
             };
             reader.onerror = function(event) {
                 alert("ERROR: " + event.target.error.code);
@@ -158,5 +172,206 @@ $( document ).ready(function() {
     }
     
     
+    /* triggers for checkALL function */
+    $("#sendBut").click(function(){
+        clickedToSubmit=1;
+        checkAll();
+    })
+    
+    $("body").mouseup(function(){
+        if(checkedOnce>0 && checkVal>0 ){
+            clickedToSubmit=0;
+            checkAll();      
+        }
+    })
+    
+    
+    function checkAll(){
+        // this function checks for all form values and makes json string to post or alerts user to complete fields.
+        console.log("checking form...");
+        checkVal = 0;
+        var asset_Id = $('#movieID').val();
+        var original_Title = $('#orginalTitle').val();
+        var canal_selected;
+        var director_selected = $('#director').val();
+        var elenco_selected = $('#elenco').val();
+        var year_selected = $('#releaseYear').val();
+        
+        // chequea original Title
+        if(original_Title=="" || original_Title==" ")
+        {
+            errorMe("#orginalTitle");
+            checkVal++;
+        }else{
+            okMe("#orginalTitle");
+        }
+        // chequea thumbnail horizaontal
+        if(!$('#ThumbHor').val()){
+            errorMe("#ThumbHor");
+            checkVal++;
+        }else{
+            okMe("#ThumbHor");
+        }
+        
+        // chequea thumbnail vertical
+        if(!$('#ThumbVer').val()){
+            errorMe("#ThumbVer");
+            checkVal++;
+        }else{
+            okMe("#ThumbVer");
+        }
+        
+        // chequea pornstar
+        if ( $('#pornstarDrop li').length < 1 )
+        {
+            errorMe("#pornstarDrop");
+            checkVal++;
+        }else{
+            okMe("#pornstarDrop");
+        }
+        
+        // chequea categorías
+        if ( $('#generoDrop li').length < 1 )
+        {
+            errorMe("#generoDrop");
+            checkVal++;
+        }else{
+            okMe("#generoDrop");
+        }
+        
+        // chequea canal
+        if ( $('#canalSelect').val()=="")
+        {
+            errorMe("#canalSelect");
+            checkVal++;
+        }else{
+            okMe("#canalSelect");
+            canal_selected=$('#canalSelect').val();
+        }
+        
+        // chequea director
+        if(director_selected=="" || director_selected==" ")
+        {
+            errorMe("#director");
+            checkVal++;
+        }else{
+            okMe("#director");
+        }
+        
+        // chequea año de estreno
+        if(year_selected=="" || year_selected==" ")
+        {
+            errorMe("#releaseYear");
+            checkVal++;
+        }else{
+            okMe("#releaseYear");
+        }
+        
+        // chequea elenco
+        if(elenco_selected=="" || elenco_selected==" ")
+        {
+            errorMe("#elenco");
+            checkVal++;
+        }else{
+            okMe("#elenco");
+        }
+        
+        
+                
+        
+        if(checkVal>0){
+            if(checkedOnce<1){// envía por primera vez y tiene error
+                $("#myModal2").modal();
+            }
+            console.log("checkVal:"+checkVal);
+        }else{
+            if(checkedOnce<1){// envía por primera vez y NO tiene error
+                submitJson();
+            }else{// se ha verificado más de una vez y ahora no tiene error
+                if(clickedToSubmit==1)// sólo se envía si se ha oprimido el botón de submit;
+                    {
+                        submitJson();
+                    }
+            }
+        }
+        
+        checkedOnce++;
+        
+        // checkboxes
+        /*
+        $('.chkbx').each(function () {
+            var checkBox = $(this);
+            if (checked) {
+                checkBox.prop('checked', true);
+            } else {
+                checkBox.prop('checked', false);
+                alert("chequeame una maestro")
+            }
+        });
+        */
+        
+        
+        
+        
+        // helper subfunctions
+        
+        
+            function errorMe(theField){
+                if ($(theField).parent().hasClass('has-success')){
+                    $(theField).parent().removeClass('has-success');
+                }
+                
+                if($(theField).next(".glyphicon").hasClass('glyphicon-ok')){
+                    $(theField).next(".glyphicon").removeClass('glyphicon-ok');
+                }
+                $(theField).parent().addClass('has-error');
+                $(theField).next(".glyphicon").addClass('glyphicon-remove');
+                
+            }
+        
+            function okMe(theField){
+                if ($(theField).parent().hasClass('has-error')){
+                    $(theField).parent().removeClass('has-error');
+                }
+                
+                if($(theField).next(".glyphicon").hasClass('glyphicon-remove')){
+                    $(theField).next(".glyphicon").removeClass('glyphicon-remove');
+                }
+                $(theField).parent().addClass("has-success");
+                $(theField).next(".glyphicon").addClass("glyphicon-ok");
+                
+            }
+        
+        
+            function submitJson(){
+                if(checkVal==0){
+                    var myJSON = '';
+                    myJSON+='{"Movie":{';
+                    myJSON+='"asset_id":"'+asset_Id+'",';
+                    myJSON+='"original_title":"'+original_Title+'",';
+                    myJSON+='"channel_id":"2",';
+                    myJSON+='"year":'+year_selected+',';
+                    myJSON+='"girls": [{"girl_id":"1"},{"girl_id":"2"}],';
+                    myJSON+='"cast":"'+elenco_selected+'",';
+                    myJSON+='"directors":"'+director_selected+'",';
+                    myJSON+='"display_runtime": "20:30",';
+                    myJSON+='"categories": [{"category_id":"1"},{"category_id":"2"}],';
+                    myJSON+='"Moviesmetadata": [{"Moviemetadata":';
+                    myJSON+='{"language": "es",';
+                    myJSON+='"title": "title 1 español",';
+                    myJSON+='"summary_short": "summary_short 1 ",';
+                    myJSON+='"summary_long":"summary_long 1 "}},';
+                    myJSON+='{"Moviemetadata":{"language": "pt",';
+                    myJSON+='"title": "title 2 portugues",';
+                    myJSON+='"summary_short": "summary_short 2 ",';
+                    myJSON+='"summary_long":"summary_long 2 "}}'; 
+                    myJSON+=']}}';
+                    console.log(myJSON);
+                    $("#varsToJSON").val(myJSON);
+                    $("#movieForm").submit();
+                  }  
+            }
+        
+    }
     
 });
